e5c13e5c4ddfd9120b18b6e0c46ae4f407247aa3
Fabian <lfotalora08@gmail.com>
2023-09-21 21:49:30 -0500

    V1 terminada (cambios para subir 10)

--- resources/views/profesor/perfil.blade.php
+++ resources/views/profesor/perfil.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>Perfil</title>
     <link rel="stylesheet" href="{{asset('css/header_nav_encabezado.css')}}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
